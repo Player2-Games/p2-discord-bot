485671d8660502622c76f24927f3704f6de959ef
Kauã de Oliveira <98750284+kauadeoliveira@users.noreply.github.com>
2024-02-18 00:09:53 -0300

    feat: Adicionar Chatbot de Discord conectado à API da OpenAI - Implementar um bot simples de Discord que se conecta à API da OpenAI ChatGPT para interações de bate-papo. - Incluir script principal 'discord_bot.py' com funcionalidade básica de bate-papo.

---
+++ main.py
@@ -0,0 +1,70 @@
+import discord
+import openai
+from dotenv import load_dotenv
+from discord.ext import commands
+import os
+
+
+load_dotenv()
+
+DISCORD_BOT_TOKEN = os.getenv("DISCORD_BOT_TOKEN")
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+PROMPT = os.getenv("PROMPT")
+
+intents = discord.Intents.default()
+intents.messages = True
+bot = commands.Bot(command_prefix="!", intents=intents)
+
+openai.api_key=OPENAI_API_KEY
+
+
+# Função que busca as mensagens do discord
+async def get_channel_history(channel, limit=5):
+    messages_list = []
+    async for message in channel.history(limit=limit):
+        messages_list.append(
+            {
+                "role": "user" if message.author.id != bot.user.id else "system",
+                "content": message.content
+            }
+        )
+        
+    messages_list.reverse()
+    return messages_list
+
+# Função que faz requisição para o chat gpt  com as mensagens do discord e retorna a resposta do assistent
+def request_gpt(messages):
+    prompt = {
+        "role": "system",
+        "content": PROMPT
+    }
+    
+    messages.insert(0, prompt)
+    
+    response = openai.chat.completions.create(
+        messages=messages,
+        model="gpt-3.5-turbo-16k",
+        temperature=0.9,
+        max_tokens=3000,
+    )
+    
+    return response.choices[0].message.content
+
+@bot.event
+async def on_message(message):
+    if message.author.bot:
+        return
+    
+    async with message.channel.typing():
+        # Busca um array com as ultimas 5 mensagens do discord (bot e usuario)
+        messages = await get_channel_history(message.channel)
+        
+        # Faz requisição pra api com as mensagens do discord
+        response = request_gpt(messages)
+
+        # Responde a mensagem com a resposta do gpt
+        await message.reply(response)
+        
+    await bot.process_commands(message)
+
+bot.run(DISCORD_BOT_TOKEN)
